399662efb68b72a7cfe26f6ce062f0f753db68d6
Lemii <peter.spaargaren@gmail.com>
2019-10-24 14:32:01 +0200

    polish: change padding for smaller screens

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -5,10 +5,16 @@ import Routes from "./Routes";
 import "./Icons";
 
 const App = () => {
+  const isSmallScreen = window.innerWidth < 768;
+
   return (
     <div>
       <Header />
-      <div className="container bg-light text-center p-5">
+      <div
+        className={`container bg-light text-center pt-5 pb-5 ${
+          isSmallScreen ? "pl-3 pr-3" : "pl-5 pr-5"
+        }`}
+      >
         <Routes />
         <Footer />
       </div>
